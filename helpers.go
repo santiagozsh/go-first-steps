5ebc3ece5822cbb5fe1211dd11b92a22cb1f9de3
santiago <sanrodsuz@pm.me>
2026-09-10 22:22:35 -0500

    add: array, structs, pointer, maps, interfaces, annonymous funcs

--- helpers.go
+++ helpers.go
@@ -69,3 +69,69 @@ func switchcase() {
 		fmt.Println("Good evening.")
 	}
 }
+
+func deferCase() {
+	// defer is really hande when it comes to close a database for avoid memory leaks
+	// dbOpen := true
+	// defer closedb()
+}
+
+func getArray() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+
+	integers := [6]int{1, 2, 3, 4, 5, 6}
+	fmt.Println(integers)
+}
+
+func getSlice() {
+	var a []int
+	c := make([]int, 5)
+
+	b := []any{1, 2, 3, "helloo"}
+
+	b = append(b, "world")
+	fmt.Println(a, b[0:2], c)
+
+	myslice := make([]int, 5)
+	myslice[0] = 1
+	myslice[1] = 1
+	myslice[2] = 1
+
+	for i, v := range myslice {
+		fmt.Printf("index %v , value %v\n", i, v)
+	}
+}
+
+func getMap() {
+	m := make(map[string]int)
+
+	m1 := map[string]int{}
+
+	m["key1"] = 42
+	m1["key1"] = 32
+
+	delete(m, "key1")
+
+	v, ok := m["key1"]
+	fmt.Println("the value:", v, "Present?", ok)
+
+	for key, value := range m1 {
+		fmt.Println(key, value)
+	}
+}
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 2)
+}
+
+func annonymusfn() {
+	hole := func(x, y float64) float64 {
+		return x + y
+	}
+
+	fmt.Println(hole(3, 4))
+	fmt.Println(compute(hole))
+}
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // this is the only way to initialize outside a func
-var (
-	name string = "santiago"
-	py   bool
-)
+// var (
+// 	name string = "santiago"
+// 	py   bool
+// )
+
+// struct arregment collects fields with different data types
+type Vertex struct {
+	X int
+	Y int
+	// common way to use is for parsing data from json into our struct, capital letter at first for given permission to be used outside the pacakge
+	// Post string `json:"post_id"`
+	// this cannot be used outside the pacakge1
+	// getX int
+}
+
+type Money string
+
+// always use the same patter with * or without it for prevent bugs
+func (v *Vertex) Abs() int {
+	return v.X + v.Y
+}
+
+func (m Money) ItsUsd() bool {
+	return m == "USD"
+}
 
 func main() {
-	fmt.Printf("Type: %t String: %q", py, name)
+	v := Vertex{2, 3}
+	fmt.Println(v.Abs())
 }
